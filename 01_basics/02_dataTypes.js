b17a9db6ca73a6bf4ff74a7f1a2dc01d93527b48
Teesha5667 <teeshasahu89@gmail.com>
2026-04-20 07:12:11 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js , not browser
+
+console.log(3
+    +3
+);  // code readability should be high 
+
+console.log("Teesha")
+
+let name = "Teesha"
+let age = 23
+let isLoggedIn = false
+let state;
+let city = null;
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof undefind) // undefined
