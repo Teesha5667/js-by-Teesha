52d84d6eba21629c4e836b9f51a3c4097f584664
Teesha5667 <teeshasahu89@gmail.com>
2026-04-20 07:33:47 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Teesha"
+
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
